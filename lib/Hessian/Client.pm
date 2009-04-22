5bd11fdeb859aa5040dde4106ce59dbba3621827
William Travis Holton <heytrav@cpan.org>
2009-04-22 18:49:59 +0000

    Import of HEYTRAV/Hessian-Client-0.1.15 from CPAN.
    gitpan-cpan-distribution: Hessian-Client
    gitpan-cpan-version:      0.1.15
    gitpan-cpan-path:         HEYTRAV/Hessian-Client-0.1.15.tar.gz
    gitpan-cpan-author:       HEYTRAV
    gitpan-cpan-maturity:     released

--- lib/Hessian/Client.pm
+++ lib/Hessian/Client.pm
@@ -3,7 +3,7 @@ package Hessian::Client;
 use strict;
 use warnings;
 
-use version; our $VERSION = qv('0.1.14');
+use version; our $VERSION = qv('0.1.15');
 
 use LWP::UserAgent;
 use HTTP::Request;
